46a111bea619f23d139b130c8c7500c3cb687b38
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-03-12 18:41:22 +0300

    add tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
+  <h1 class="bg-indigo-400 text-indigo-700">Hello world!</h1>
+
   <div>
     <NuxtWelcome />
   </div>
+
 </template>
+
+<style>
+h1 {
+  @apply  text-3xl font-bold ;
+}
+
+</style>
+

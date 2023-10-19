c8954adb93868c879ca002ae285d2b6e706c5ad9
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-10-19 22:40:01 +0300

    validate example

---
+++ pages/contacts.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="bg-indigo-400 text-indigo-700">Hello world!</h1>
+
+  <div>
+    hui
+  </div>
+</template>
+<script>
+  definePageMeta({
+  validate:  (route) => {
+    return false
+  }
+})
+</script>
